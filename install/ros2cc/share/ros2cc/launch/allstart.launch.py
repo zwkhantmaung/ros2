0c3b9547cd93c651802ca9e01a229aa282e8f5d3
zwkhantmaung <zawkhantmaung@outlook.com>
2024-11-14 23:20:38 +0900

    .

--- install/ros2cc/share/ros2cc/launch/allstart.launch.py
+++ install/ros2cc/share/ros2cc/launch/allstart.launch.py
@@ -13,5 +13,25 @@ def generate_launch_description():
             package='turtlesim',
             name='turtle2'
         ),
+        Node(
+            executable='talker_int32',
+            package='ros2cc',
+            name='talker_int32'
+        ),
+        Node(
+            executable='listener_int32',
+            package='ros2cc',
+            name='listener_int32'
+        ),
+        Node(
+            executable='talker_str',
+            package='ros2cc',
+            name='talker_str'
+        ),
+        Node(
+            executable='listener_str',
+            package='ros2cc',
+            name='listener_str'
+        ),
     ])
 
